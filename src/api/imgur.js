c86720e0d4e402ac103dbdd401445a86b995ac95
huashenliang <JohnnyLiang77@gmail.com>
2020-06-03 13:18:05 -0500

    added api to fetch images

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,8 +1,8 @@
 import qs from 'qs';
+import axios from 'axios';
 
-const CLIENT_ID = 'ID HERE'
+const CLIENT_ID = ''
 const ROOT_URL = 'https://api.imgur.com'
-
 
 export default {
     login(){
@@ -11,5 +11,13 @@ export default {
             response_type: 'token'
         };
        window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(queryString)}`;
+    },
+
+    fetchImages(token) {
+        return axios.get(`${ROOT_URL}/3/account/me/images`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
     }
 }
