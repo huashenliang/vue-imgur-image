4172f2ec24186dcd845dc0a0b85328f07bbf0e20
huashenliang <JohnnyLiang77@gmail.com>
2020-06-03 11:54:40 -0500

    able to login, logout, nav to different components

--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@ import App from './App.vue'
 import store from './store';
 import VueRouter from 'vue-router';
 import AuthHandler from './components/AuthHandler';
+import ImageList from './components/ImageList';
+import UploadForm from './components/UploadForm';
 
 Vue.config.productionTip = false
 Vue.use(VueRouter);
 
-const router = new VueRouter({
+export const router = new VueRouter({
   mode:'history',
   routes: [
+    {path: '/', component: ImageList},
+    {path: '/upload', component: UploadForm},
     {path: '/oauth2/callback', component: AuthHandler }
   ]
 })
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,8 +1,9 @@
 import api from '../../api/imgur';
 import qs from 'qs';
+import {router} from '../../main';
 
 const state = {
-    token: null
+    token: window.localStorage.getItem('imgur_token')
 };
 
 const getters = {
@@ -15,11 +16,15 @@ const actions = {
     },
     logout: ({commit}) => {
         commit('setToken', null);
+        window.localStorage.removeItem('imgur_token');
+        router.push('/');
     },
     finalizeLogin: ({commit}, hash) => {
         const queryString = qs.parse(hash.replace('#', ''));
 
         commit('setToken', queryString.access_token);
+        window.localStorage.setItem('imgur_token', queryString.access_token);
+        router.push('/');
     }
 };
 
